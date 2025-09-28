f55955824df4fd1da0ff7f00d50d14156088d5b8
vivekphull <phull.vivek@gmail.com>
2025-09-28 21:15:32 +0930

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+// Load posts on homepage
+if (document.getElementById('blog-list')) {
+  fetch('posts.json')
+    .then(res => res.json())
+    .then(posts => {
+      const container = document.getElementById('blog-list');
+      container.innerHTML = '';
+      posts.forEach(post => {
+        container.innerHTML += `
+          <article>
+            <h2><a href="post.html?id=${post.id}">${post.title}</a></h2>
+            <p><em>${post.date}</em></p>
+            <p>${post.summary}</p>
+          </article>
+        `;
+      });
+    });
+}
+
+// Load individual post
+if (document.getElementById('post-content')) {
+  const params = new URLSearchParams(window.location.search);
+  const postId = params.get('id');
+
+  fetch('posts.json')
+    .then(res => res.json())
+    .then(posts => {
+      const post = posts.find(p => p.id === postId);
+      const container = document.getElementById('post-content');
+      if (post) {
+        container.innerHTML = `
+          <h2>${post.title}</h2>
+          <p><em>${post.date}</em></p>
+          ${post.content}
+        `;
+      } else {
+        container.innerHTML = '<p>Post not found.</p>';
+      }
+    });
+}
