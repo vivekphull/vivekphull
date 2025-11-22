a25073ec5604415c066cd55545cfd2a61a3857a4
vivekphull <phull.vivek@gmail.com>
2025-11-22 18:26:29 +1030

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -55,4 +55,4 @@ export default async function handler(req, res) {
       message: error.message
     });
   }
-}
+};
